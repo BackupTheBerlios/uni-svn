ee390f83e488f716fe1a698fd3b2636fb94c3a91
chentianran <chentianran@5863635e-bdf4-0310-8ea1-da5d298f4d5d>
2005-06-17 19:51:45 +0000

    * allow any strings to be used as open and closed symbol * allow user choose scanners from command line option

--- trunk/filter/unifilt.cc
+++ trunk/filter/unifilt.cc
@@ -9,38 +9,78 @@
 #include <sstream>
 #include <fstream>
 
+#define DEFAULT_OPEN_SYM   "{"
+#define DEFAULT_CLOSE_SYM  "}"
+#define DEFAULT_SCANNER_NAME "curly-ascii"
+
 using namespace NAMESPACE;
 using namespace std;
 
-void filt (Context* context, Scanner* scanner, istream& input, ostream& output, ostream& error)
+bool read_until (istream& input, ostream& output, string sym)
 {
-  int x;
+  string::iterator oi = sym.begin();
+  stringstream buf;
 
-  while (-1 != (x = input.get())) {
-    if ('{' != x)
-      output << static_cast<char>(x);
-    else {
-      stringstream ss;
-      while (input.good()) {
-	char y = input.get();
-	if ('}' == y)
-	  break;
-	ss.put (y);
-      }
-      if (ss.good()) {
-	RawPtr raw = scanner->scan (context, ss, error);
-	TermPtr result = context->eval (raw, ALL);
-
-	TermPtr str = Tok::create ("str");
-	TermPtr app_result = App::create (str,result);
-
-	result = context->reduce_in_shield (app_result);
-
-	StrPtr str_result = CAST<Str> (result);
-
-	output << str_result->str();
-      }
+  for (int x = input.get(); x != -1; x = input.get()) {
+    if (*oi == x) {
+      do {
+	if (*(oi++) == x)
+	  buf.put (x);
+	else if ('\0' == *oi) {
+	  input.unget();
+	  return true;
+	}
+	else {
+	  if (buf.good())
+	    output << buf;
+	  buf.clear();
+	  oi = sym.begin();
+	}
+      } while (-1 != (x = input.get()));
     }
+    else
+      output.put (x);
+  }
+
+  return false;
+}
+
+void filt (Context* context,
+	   Scanner* scanner,
+	   istream& input,
+	   ostream& output,
+	   ostream& error,
+	   const string&  open,
+	   const string&  close)
+{
+  stringstream buf;
+
+  for ( ; ; ) {
+    if (read_until (input, output, open)) {
+      if (input.good()) {
+	if (read_until (input, buf, close)) {
+	  if (buf.good()) {
+ 	    RawPtr raw = scanner->scan (context, buf, error);
+ 	    TermPtr result = context->eval (raw, ALL);
+	    
+ 	    TermPtr str = Tok::create ("str");
+ 	    TermPtr app_result = App::create (str,result);
+
+ 	    result = context->reduce_in_shield (app_result);
+
+ 	    StrPtr str_result = CAST<Str> (result);
+
+ 	    output << str_result->str();
+	  }
+	}
+	else
+	  error << "[ERROR] imcomplete open-close pair";
+      }
+      else
+	error << "[ERROR] imcomplete open-close pair";
+    }
+    else
+      break;
   }
 }
 
@@ -67,15 +107,42 @@ Scanner* load_scanner (const string& name)
     throw dlerror();
 }
 
-int main (int argc, int argv)
+int main (int argc, char** argv)
 {
   Context context;
   Builtin builtin;
 
+  const char* open = 0;
+  const char* close = 0;
+  const char* scanner = 0;
+
+  for (int c; (c = getopt (argc, argv, "b:e:s:")) != -1; ) {
+    switch (c) {
+    case 'b':
+      open = optarg;
+      break;
+    case 'e':
+      close = optarg;
+      break;
+    case 's':
+      scanner = optarg;
+      break;
+    default:
+      abort();
+    }
+  }
+
+  if (0 == open)
+    open = DEFAULT_OPEN_SYM;
+  if (0 == close)
+    close = DEFAULT_CLOSE_SYM;
+  if (0 == scanner)
+    scanner = DEFAULT_SCANNER_NAME;
+
   try {
-    Scanner* s = load_scanner("curly-ascii");
+    Scanner* s = load_scanner(scanner);
     builtin.init (&context);
-    filt (&context, s, cin, cout, cerr);
+    filt (&context, s, cin, cout, cerr, open, close);
   }
   catch (const char* err) {
     cerr << err << endl;
